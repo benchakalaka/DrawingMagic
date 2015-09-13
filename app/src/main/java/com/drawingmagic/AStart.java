6249cfe5d2334af8a93df2cde1143bef853bf07b
benchahakalaka <benchakalaka@gmail.com>
2015-09-13 14:23:06 +0100

    Added support for AA

---
+++ app/src/main/java/com/drawingmagic/AStart.java
@@ -0,0 +1,17 @@
+package com.drawingmagic;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.EApplication;
+
+@EActivity(R.layout.activity_start)
+public class AStart extends ActionBarActivity {
+
+
+
+
+}
